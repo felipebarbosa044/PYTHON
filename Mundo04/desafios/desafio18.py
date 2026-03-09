9afc753fcbfc833310eb44d9d5599c688ea347ef
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-03-09 20:57:20 -0300

    Desafio 19 Completo

--- Mundo04/desafios/desafio18.py
+++ Mundo04/desafios/desafio18.py
@@ -1,13 +1,20 @@
 from rich import print
 from rich.panel import Panel
 
+
+
 class Churrasco:
     def __init__(self,nome,quantidade = 0):
         self.nome = nome
         self.quantidade = quantidade
 
     def analisar(self):
-        churrasco = Panel(f"Analisando [green]{self.nome}[/] com {self.quantidade} convidados")
+        kg = self.quantidade * 0.4
+        total_RS = kg * 82.40
+        total_Participante =  total_RS / self.quantidade
+
+        churrasco = Panel(f"Analisando [green]{self.nome}[/] com [blue]{self.quantidade} convidados[/]\nCada participante comerá 0.4Kg e cada Kg custa R$82.40\nRecomendado [blue]comprar {kg:.3f}Kg[/] de carne\nO custo total será de [green]R${total_RS:.2f}[/]\nCada pessoa pagará [yellow]R${total_Participante}[/] para participar.",title=f"{self.nome}")
         print(churrasco)
 
-churrasco = Churrasco("Churras dos Amigos")
+churrasco = Churrasco("Churras dos Amigos",15)
+churrasco.analisar()
